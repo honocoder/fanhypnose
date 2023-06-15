531d4286efeeec416077963609fd7c9da8c70da5
Jimy Marletta <84329916+jimymltta@users.noreply.github.com>
2023-06-15 11:27:56 +0200

    add header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,9 @@
 import { defineConfig } from 'astro/config';
-import storyblok from '@storyblok/astro';
-import { loadEnv } from 'vite';
+import tailwind from "@astrojs/tailwind";
 
-const env = loadEnv('', process.cwd(), 'STORYBLOK');
+import svelte from "@astrojs/svelte";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    storyblok({
-      accessToken: env.STORYBLOK_TOKEN,
-      components: {},
-      apiOptions: {
-        region: 'eu',
-      },
-    }),
-  ],
+  integrations: [tailwind(), svelte()]
 });
