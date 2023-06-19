d1f32735020f609f24876a66fbebf8558441e09c
Jimy Marletta <84329916+jimymltta@users.noreply.github.com>
2023-06-19 15:54:53 +0200

    add testimonials

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,15 +3,7 @@ const plugin = require('tailwindcss/plugin');
 
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {
-      textShadow: {
-        sm: '0 1px 2px var(--tw-shadow-color)',
-        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
-        lg: '0 8px 16px var(--tw-shadow-color)',
-      },
-    },
-  },
+  theme: {},
   plugins: [
     plugin(function ({ matchUtilities, theme }) {
       matchUtilities(
