d4097b58abb820a901709e73b52db5eea0500c27
Jimy Marletta <84329916+jimymltta@users.noreply.github.com>
2023-06-20 15:10:53 +0200

    clean astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import image from '@astrojs/image';
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), react()]
+  integrations: [tailwind(), image()],
 });
