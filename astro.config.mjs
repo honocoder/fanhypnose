6c0c40b42103f6c9a642474b9a4dae486282f215
Jimy Marletta <84329916+jimymltta@users.noreply.github.com>
2023-06-20 23:03:35 +0200

    change faq design and make the header better

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import image from '@astrojs/image';
-
 import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
@@ -9,6 +8,6 @@ export default defineConfig({
   integrations: [tailwind(), image()],
   output: 'static',
   adapter: vercel({
-    analytics: true,
-  }),
+    analytics: true
+  })
 });
