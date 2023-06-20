b4a2a987ee84e54be45be267f32a9931a184af1d
Jimy Marletta <84329916+jimymltta@users.noreply.github.com>
2023-06-20 16:59:54 +0200

    fix astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import image from '@astrojs/image';
 
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
