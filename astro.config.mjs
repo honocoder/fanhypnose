f367ccd4c927fde86bffb7cdf5642ba7927366c5
Jimy Marletta <84329916+jimymltta@users.noreply.github.com>
2023-06-20 16:58:57 +0200

    add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import image from '@astrojs/image';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), image()],
+  output: 'static',
+  adapter: vercel({
+    analytics: true,
+  }),
 });
